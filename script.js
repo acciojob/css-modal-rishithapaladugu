a0c4911e8b900406a64e852394b11753461eae51
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-07-30 19:47:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+document.getElementById("openModal").onclick = function() {
+    document.getElementById("modal").style.display = "flex";
+};
+
+document.querySelector(".close-modal").onclick = function() {
+    document.getElementById("modal").style.display = "none";
+};
+
+window.onclick = function(event) {
+    const modal = document.getElementById("modal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
