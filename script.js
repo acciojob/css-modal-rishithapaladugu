410c52fd2a6a641ecaf4ae5dae6448de15c2eae7
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-07-31 19:56:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 //your JS code here. If required.
-document.getElementById("openModal").onclick = function() {
-    document.getElementById("modal").style.display = "flex";
-};
 
-document.querySelector(".close-modal").onclick = function() {
-    document.getElementById("modal").style.display = "none";
-};
+const express = require('express');
+const path = require('path');
 
-window.onclick = function(event) {
-    const modal = document.getElementById("modal");
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-};
+const app = express();
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/main.html'));
+});
+//your code here
+app.post('/add', (req, res) => {
+  const {a,b} = req.body;
+  res.status(200).send(a+b);
+  // res.sendFile(path.join(__dirname + '/main.html'));
+});
+module.exports = app;
